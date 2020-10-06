2996d971dfe461e26a0f3feb51db0b6ed9f215de
clearjoy17 <cj.edits17@gmail.com>
2020-10-06 15:42:18 +0800

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Demo</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+     <style>
+        body{
+            margin: 30px;
+        }
+    </style>
+</head>
+<body>
+    
+        <h1>Laravel Demo</h1>
+
+        <ul class="nav justify-content-end float-right nav-pills">
+            <li class="nav-item">
+                <a class="nav-link " href="{{url('/')}}">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="{{url('/brand')}}">Brand</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="{{url('/about')}}">About</a>
+            </li>
+        </ul>
+    
+    
+</body>
+</html>
